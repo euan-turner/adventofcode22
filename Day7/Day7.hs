831746f692707bfb82ebc8654762b62cd018a45d
Euan Turner <euanoturner@gmail.com>
2022-12-07 08:23:54 +0000

    Almost there

---
+++ Day7/Day7.hs
@@ -0,0 +1,77 @@
+module Main where 
+
+import Data.Maybe 
+
+type Directory = String
+type Size = Int
+type SubDirectoryMap = [(Directory, [Directory])]
+type DirectoryFileSize = [(Directory, Size)]
+
+-- Returns first occurrence
+lookUp :: Directory -> a -> [(Directory, a)] -> a
+lookUp k def kvs
+  | null fil = def 
+  | otherwise = head fil
+  where 
+    fil = [v | (k', v) <- kvs, k == k']
+
+buildDirectory :: [String] -> DirectoryFileSize -> SubDirectoryMap -> (DirectoryFileSize, SubDirectoryMap)
+buildDirectory [] fs sm = (fs, sm)
+buildDirectory (('$':' ':'c':'d':' ':dir):ls) fs sm
+  | dir == ".." = buildDirectory ls fs sm
+  | otherwise = buildDirectory ls fs' sm'
+  where 
+    fs' = fs ++ [(dir, directoryFileSize ls)]
+    sm' = sm ++ [(dir, directorySubdirectories ls)]
+buildDirectory (_:ls) fs sm = buildDirectory ls fs sm
+
+-- called after ls, should go until next cd
+directoryFileSize :: [String] -> Size
+directoryFileSize [] = 0
+directoryFileSize (('$':' ':'c':'d':' ':dir):cs)  = 0
+directoryFileSize (('d':'i':'r':dir):cs) = directoryFileSize cs
+directoryFileSize (file:cs) = (read size :: Size) + directoryFileSize cs
+  where 
+    size = (head . words) file
+
+directorySubdirectories :: [String] -> [Directory]
+directorySubdirectories [] = []
+directorySubdirectories (('$':' ':'c':'d':' ':dir):ls) = []
+directorySubdirectories (('d':dir):ls) = (last . words) dir : directorySubdirectories ls
+directorySubdirectories (_:ls) = directorySubdirectories ls
+
+transitiveSubDirectories :: SubDirectoryMap -> SubDirectoryMap
+transitiveSubDirectories [] = []
+transitiveSubDirectories ((dir, sdirs):sm) = (dir, sdirs ++ sdirs') : sm'
+  where 
+    sm' = transitiveSubDirectories sm
+    sdirs' = concatMap (\sd -> lookUp sd [] sm') sdirs
+
+transitiveFileSize :: SubDirectoryMap -> DirectoryFileSize -> DirectoryFileSize
+transitiveFileSize _ [] = []
+transitiveFileSize sm ((dir, size): fs) = (dir, size + size') : fs'
+  where 
+    fs' = transitiveFileSize sm fs 
+    sdirs = lookUp dir [] sm 
+    size' = sum (map (\sd -> lookUp sd 0 fs) sdirs)
+
+showSM :: SubDirectoryMap -> String
+showSM [] = ""
+showSM ((dir, dirs) : sm) = dir ++ ": " ++  show dirs ++ "\n" ++ showSM sm 
+
+showFS :: DirectoryFileSize -> String 
+showFS [] = ""
+showFS ((dir, size): fs) = dir ++ ": " ++ show size ++ "\n" ++ showFS fs
+
+main :: IO ()
+main = do 
+  input <- readFile "files.txt"
+  let ls = lines input 
+  let ls' = filter (/= "$ ls") ls
+  let (fs, sm) = buildDirectory ls' [] []
+  putStrLn (showFS fs)
+  let sm' = transitiveSubDirectories sm
+  let fs' = transitiveFileSize sm' fs
+  let less = filter (\(_, s) -> s <= 100000) fs'
+  let total = sum (map snd less)
+  putStrLn (show total)
