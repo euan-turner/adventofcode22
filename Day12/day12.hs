8a74857798fe6e03d42ea9a2fe15fc9b6351a064
Euan Turner <euanoturner@gmail.com>
2022-12-12 08:37:16 +0000

    Need A*

--- Day12/day12.hs
+++ Day12/day12.hs
@@ -1,5 +1,6 @@
 module Main where 
 
+import Data.Char ( ord )
 type Pos = (Int, Int)
 
 --Find start position
@@ -40,26 +41,35 @@ traverse (x, y) ls = undefined
         right = if succ c <= w then traverse' (x + 1, y) (xy:xys) w north south (w:east) ws else []
         paths = up ++ down ++ left ++ right 
 
-traverse2 :: Pos -> [Pos] -> [String] -> [[Pos]]
+traverse2 :: Pos -> [Pos] -> [[Int]] -> Int
 traverse2 xy@(x, y) xys ls
-  | ls !! y !! x == 'E' = [[xy]]
-  | xy `elem` xys = []
-  | otherwise = concatMap (xy:) (left ++ right ++ up ++ down)
+  | ls !! y !! x == 26 = 0
+  | xy `elem` xys = 10000
+  | otherwise = 1 + minimum [left, right, up , down]
   where 
-    c = ls !! y !! x
-    width = length (ls !! 0)
-    height = length ls
-    left = if (x > 0) && (ls !! y !! (x - 1) <= succ c) then traverse2 (x - 1, y) (xy:xys) ls else []
-    right = if (x < width) && (ls !! y !! (x + 1) <= succ c) then traverse2 (x + 1, y) (xy:xys) ls else []
-    up = if (x > 0) && (ls !! (y - 1) !! x <= succ c) then traverse2 (x, y - 1) (xy:xys) ls else []
-    down = if (x < height) && (ls !! (y + 1) !! x <= succ c) then traverse2 (x, y + 1) (xy:xys) ls else []
+    h = ls !! y !! x
+    width = length (ls !! 0) - 1
+    height = length ls - 1
+    left = if (x > 0) && (ls !! y !! (x - 1) <= succ h) then traverse2 (x - 1, y) (xy:xys) ls else 10000
+    right = if (x < width) && (ls !! y !! (x + 1) <= succ h) then traverse2 (x + 1, y) (xy:xys) ls else 10000
+    up = if (y > 0) && (ls !! (y - 1) !! x <= succ h) then traverse2 (x, y - 1) (xy:xys) ls else 10000
+    down = if (y < height) && (ls !! (y + 1) !! x <= succ h) then traverse2 (x, y + 1) (xy:xys) ls else 10000
 
 
+formatLine :: String -> [Int]
+formatLine [] = []
+formatLine ('S':cs) = 0 : formatLine cs
+formatLine ('E':cs) = 26 : formatLine cs 
+formatLine (c:cs) = ord c - ord 'a' : formatLine cs
 
 main :: IO ()
 main = do
-  input <- readFile "sample.txt"
+  input <- readFile "hills.txt"
   let ls = lines input
   let start = findStart ls 0
-  let paths = traverse2 start [] ls
-  putStrLn(show paths)
+  let f = map formatLine ls
+  let dist = traverse2 start [] f
+  putStrLn (show dist)
+
+-- May need to do A*
+-- https://www.geeksforgeeks.org/a-search-algorithm/
