51f909631aae1abc03890892b13687767349125a
Euan Turner <euanoturner@gmail.com>
2022-12-13 09:33:39 +0000

    Part one

---
+++ Day13/day13.py
@@ -0,0 +1,56 @@
+
+def format(file):
+  ls = []
+  for line in file:
+    if line == "\n":
+      continue 
+    else:
+      ls.append(eval(line))
+  return ls
+
+def pairUp(packets: list[list[int]]) -> list[tuple[int]]:
+  ps = zip(packets[::2], packets[1::2])
+  return ps 
+
+def compare(left, right):
+  if (type(left) == int and type(right) == int):
+    if left < right:
+      return 1
+    elif left > right:
+      return -1
+    else:
+      return 0
+  elif (type(left) == list and type(right) == list):
+    for i in range(min(len(left), len(right))):
+      comp = compare(left[i], right[i])
+      if (comp != 0):
+        return comp
+    if (len(left) < len(right)):
+      return 1
+    elif (len(right) < len(left)):
+      return -1
+    else:
+      return 0
+  elif (type(right) == list):
+    return compare([left], right)
+  elif (type(left) == list):
+    return compare(left, [right])
+
+def inRightOrder(pairs: list[tuple[int]]) -> list[int]:
+  s = 0
+  inds = []
+  for i, p in enumerate(pairs):
+    if compare(*p) == 1:
+      s += i + 1
+      inds.append(i)
+  print(inds)
+  print(s)
+
+def partOne():
+  file = open("./Day13/packets.txt")
+  form = format(file)
+  pairs = pairUp(form)
+  inRightOrder(pairs)
+
+if __name__ == "__main__":
+  partOne()
